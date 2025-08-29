193efd84fb573db60e03853572c1003758d008ff
Vinicius <vinishoff@gmail.com>
2025-08-29 17:09:00 -0300

    feat: Implement movie cards section/TMDB API integration

---
+++ config.example.js
@@ -0,0 +1,4 @@
+export const config = {
+  TMDB_API_KEY: "your_key_here",
+  IMAGE_URL: "https://image.tmdb.org/t/p/w342"
+}
---
+++ src/script.js
@@ -0,0 +1,48 @@
+import { config } from "/config.js";
+
+const elementForm = document.getElementById("searchForm");
+elementForm.addEventListener("submit", async (event) => {
+  const valueInput = document.getElementById("searchInput").value.trim();
+  event.preventDefault();
+
+  if(!valueInput){
+    alert("Empty input!")
+    return
+  }
+
+  try {
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${config.TMDB_API_KEY}&query=${encodeURIComponent(valueInput)}`, {
+      method: "GET",
+      headers: {
+        'Content-Type':'application/json'
+      },
+    });
+
+    if (!response.ok){
+      throw new Error(`${response.status} - ${(await response.json()).status_message}`);
+    }
+
+    const data = await response.json();
+    if(!data.results.length){
+      alert("No results found.")
+      return
+    }
+
+    const fragment = document.createDocumentFragment();
+    const cardsSection = document.getElementById("cardsSection");
+    cardsSection.innerHTML = "";
+
+    data.results.forEach((item) => {
+      const newArticle = document.createElement("article");
+      newArticle.className = "cardMovie";
+      newArticle.innerHTML = `<h3>${item.title}</h3>`;
+      newArticle.style.background = `linear-gradient(360deg, rgba(0, 0, 0, 0.8) 10%, rgba(0, 0, 0, 0) 40%), url('${item.poster_path?config.IMAGE_URL+item.poster_path:"./images/noimage.png"}') center/cover no-repeat`;
+      fragment.appendChild(newArticle);
+    })
+
+    cardsSection.appendChild(fragment);
+
+  } catch (error) {
+    alert(error);
+  }
+})
